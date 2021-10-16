47c9d03a45b2014fefe44539f0d17286d01c7cf6
unknown <staubjoao>
2021-10-16 12:15:16 -0300

    display

--- App.js
+++ App.js
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View, StyleSheet} from 'react-native';
 import Button from './src/components/Button';
+import Display from './src/components/Display';
 
-export default () => (
-  <View style={style.container}>
-    <View style={style.button}>
-      <Button label="AC" />
-      <Button label="/" />
-      <Button label="7" />
-      <Button label="8" />
-      <Button label="9" />
-      <Button label="*" />
-      <Button label="4" />
-      <Button label="5" />
-      <Button label="6" />
-      <Button label="-" />
-      <Button label="1" />
-      <Button label="2" />
-      <Button label="3" />
-      <Button label="+" />
-      <Button label="0" />
-      <Button label="." />
-      <Button label="=" />
-    </View>
-  </View>
-);
+export default class App extends Component {
+  state = {
+    displayValue: '0',
+  };
+
+  render() {
+    return (
+      <View style={style.container}>
+        <Display value={this.state.displayValue} />
+        <View style={style.button}>
+          <Button label="AC" />
+          <Button label="/" />
+          <Button label="7" />
+          <Button label="8" />
+          <Button label="9" />
+          <Button label="*" />
+          <Button label="4" />
+          <Button label="5" />
+          <Button label="6" />
+          <Button label="-" />
+          <Button label="1" />
+          <Button label="2" />
+          <Button label="3" />
+          <Button label="+" />
+          <Button label="0" />
+          <Button label="." />
+          <Button label="=" />
+        </View>
+      </View>
+    );
+  }
+}
 
 const style = StyleSheet.create({
   container: {
