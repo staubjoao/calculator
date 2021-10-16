f0fb5b0393acdd950f56bd52c97b08408b7b715c
unknown <staubjoao>
2021-10-16 12:52:51 -0300

    buttons done

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, Dimensions, TouchableHighlight} from 'react-native';
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   button: {
     fontSize: 40,
     height: Dimensions.get('window').width / 4,
@@ -12,12 +12,26 @@ const style = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#888',
   },
+  operationButton: {
+    color: '#fff',
+    backgroundColor: '#fa8231',
+  },
+  buttonDouble: {
+    width: (Dimensions.get('window').width / 4) * 2,
+  },
+  buttonTriple: {
+    width: (Dimensions.get('window').width / 4) * 3,
+  },
 });
 
 export default props => {
+  const stylesButton = [styles.button];
+  if (props.double) stylesButton.push(styles.buttonDouble);
+  if (props.triple) stylesButton.push(styles.buttonTriple);
+  if (props.operation) stylesButton.push(styles.operationButton);
   return (
-    <TouchableHighlight onPress={props.onClick}>
-      <Text style={style.button}>{props.label}</Text>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
+      <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
